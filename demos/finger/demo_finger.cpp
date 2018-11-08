945331fafe478c9aa080a2f10a915665901d880d
manuel <manuel.wuthrich@gmail.com>
2018-11-08 14:25:15 +0100

    copied test_bench_8_motors to finger

---
+++ demos/finger/demo_finger.cpp
@@ -0,0 +1,83 @@
+/**
+ * \file demo_test_bench_8_motors.cpp
+ * \brief The use of the wrapper implementing a small pid controller.
+ * \author Maximilien Naveau
+ * \date 2018
+ *
+ * This file uses the Finger class in a small demo.
+ */
+
+#include "blmc_robots/finger/finger.hpp"
+
+using namespace blmc_robots;
+
+
+static THREAD_FUNCTION_RETURN_TYPE control_loop(void* robot_void_ptr)
+{
+  Finger& robot = *(static_cast<Finger*>(robot_void_ptr));
+
+  double kp = 5;
+  double kd = 1;
+  Vector8d desired_motor_position;
+  Vector8d desired_current;
+
+  Vector8d debug_des_pd;
+  Vector8d debug_des_u_sat;
+  Vector8d debug_des_l_sat;
+
+  Timer<10> time_logger("controller");
+  while(true)
+  {
+    // acquire the sensors
+    robot.acquire_sensors();
+
+    // the slider goes from 0 to 1 so we go from -0.5rad to 0.5rad
+    desired_motor_position = robot.get_max_range() *
+                             (robot.get_slider_positions().array() - 0.5);
+
+    // we implement here a small pd control at the current level
+    desired_current = kp * (desired_motor_position - robot.get_motor_positions()) -
+                      kd * robot.get_motor_velocities();
+
+    // Saturate the desired current
+    desired_current = desired_current.array().min(robot.get_max_current() * Vector8d::Ones().array());
+    desired_current = desired_current.array().max(-robot.get_max_current() * Vector8d::Ones().array());
+
+    // Send the current to the motor
+    robot.send_target_current(desired_current);
+
+    // print -----------------------------------------------------------
+    Timer<>::sleep_ms(1);
+
+    time_logger.end_and_start_interval();
+    if ((time_logger.count() % 1000) == 0)
+    {
+      rt_printf("sending currents: [");
+      for(int i=0 ; i<desired_current.size() ; ++i)
+      {
+        rt_printf("%f, ", desired_current(i));
+      }
+      rt_printf("]\n");
+    }
+  }//endwhile
+}// end control_loop
+
+int main(int argc, char **argv)
+{
+  Finger robot;
+
+  robot.initialize();
+
+  rt_printf("controller is set up \n");
+
+  osi::start_thread(&control_loop, &robot);
+
+  rt_printf("control loop started \n");
+
+  while(true)
+  {
+    Timer<>::sleep_ms(10);
+  }
+
+  return 0;
+}
---
+++ include/blmc_robots/finger/finger.hpp
@@ -0,0 +1,165 @@
+/**
+ * \file test_bench_8_motors.hh
+ * \brief The hardware wrapper of the Finger
+ * \author Manuel Wuthrich
+ * \date 2018
+ *
+ * This file declares the Finger class which defines the test
+ * bench with 8 motors.
+ */
+
+#pragma once
+
+
+#include <Eigen/Eigen>
+#include <blmc_robots/common_header.hh>
+
+namespace blmc_robots
+{
+
+/**
+ * @brief The Finger class implements the control of the test bench
+ * containing 8 motors and 8 sliders using the blmc drivers.
+ */
+class Finger
+{
+public:
+
+  /**
+   * @brief mi this typdef is used to get the measurments form the blmc api
+   */
+  typedef blmc_drivers::MotorInterface::MeasurementIndex mi;
+
+  /**
+   * @brief Finger is the constructor of the class.
+   */
+  Finger();
+
+  /**
+   * @brief initialize the robot by setting alining the motors and calibrate the
+   * sensors to 0
+   */
+  void initialize();
+
+  /**
+   * @brief send_target_torques sends the target currents to the motors
+   */
+  void send_target_current(
+      const Vector8d target_currents);
+
+  /**
+   * @brief acquire_sensors acquire all available sensors, WARNING !!!!
+   * this method has to be called prior to any gettter to have up to date data.
+   */
+  void acquire_sensors();
+
+  /**
+   * @brief get_motor_positions
+   * @return the current motors positions (rad).
+   */
+  const Vector8d get_motor_positions()
+  {
+    return motor_positions_;
+  }
+
+  /**
+   * @brief get_motor_velocities
+   * @return the current motors velocities (rad/s).
+   */
+  const Vector8d get_motor_velocities()
+  {
+    return motor_velocities_;
+  }
+
+  /**
+   * @brief get_motor_currents
+   * @return the current motors currents in (Amper: A).
+   */
+  const Vector8d get_motor_currents()
+  {
+    return motor_currents_;
+  }
+
+  /**
+   * @brief get_motor_encoder_indexes
+   * @return TODO: ask Manuel what is this exactly.
+   */
+  const Vector8d get_motor_encoder_indexes()
+  {
+    return motor_encoder_indexes_;
+  }
+
+  /**
+   * @brief get_slider_positions
+   * @return the current sliders positions.
+   */
+  const Vector8d get_slider_positions()
+  {
+    return slider_positions_;
+  }
+
+  /**
+   * @brief get_max_current
+   * @return the maximum current to apply to the motors
+   */
+  double get_max_current()
+  {
+    return max_current_;
+  }
+
+  /**
+   * @brief get_max_current
+   * @return the maximum current to apply to the motors
+   */
+  double get_max_range()
+  {
+    return max_range_;
+  }
+
+private:
+
+
+  Eigen::Matrix<double, 8, 1> motor_positions_;
+  Eigen::Matrix<double, 8, 1> motor_velocities_;
+  Eigen::Matrix<double, 8, 1> motor_currents_;
+  Eigen::Matrix<double, 8, 1> motor_encoder_indexes_;
+  Eigen::Matrix<double, 8, 1> slider_positions_;
+  Eigen::Matrix<double, 8, 1> target_currents;
+
+
+  std::shared_ptr<blmc_drivers::CanBus> can_bus0_;
+  std::shared_ptr<blmc_drivers::CanBus> can_bus1_;
+  std::shared_ptr<blmc_drivers::CanBus> can_bus2_;
+  std::shared_ptr<blmc_drivers::CanBus> can_bus3_;
+
+  std::shared_ptr<blmc_drivers::CanBusMotorBoard> board0_;
+  std::shared_ptr<blmc_drivers::CanBusMotorBoard> board1_;
+  std::shared_ptr<blmc_drivers::CanBusMotorBoard> board2_;
+  std::shared_ptr<blmc_drivers::CanBusMotorBoard> board3_;
+
+  double max_current_ ;
+  double max_range_ ;
+
+  // we have 4 board with each possessing 2 motors and 2 sliders
+  SafeMotor_ptr board0_motor0_;
+  SafeMotor_ptr board0_motor1_;
+  Slider_ptr board0_slider0_;
+  Slider_ptr board0_slider1_;
+
+  SafeMotor_ptr board1_motor0_;
+  SafeMotor_ptr board1_motor1_;
+  Slider_ptr board1_slider0_;
+  Slider_ptr board1_slider1_;
+
+  SafeMotor_ptr board2_motor0_;
+  SafeMotor_ptr board2_motor1_;
+  Slider_ptr board2_slider0_;
+  Slider_ptr board2_slider1_;
+
+  SafeMotor_ptr board3_motor0_;
+  SafeMotor_ptr board3_motor1_;
+  Slider_ptr board3_slider0_;
+  Slider_ptr board3_slider1_;
+};
+
+} // namespace blmc_robots
