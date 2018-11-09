58f073a4bc8743b8cfe99cb929717ca779435a7c
manuel <manuel.wuthrich@gmail.com>
2018-11-09 13:47:20 +0100

    created slider class

--- include/blmc_robots/finger/finger.hpp
+++ include/blmc_robots/finger/finger.hpp
@@ -16,6 +16,7 @@
 #include <math.h>
 
 #include <blmc_robots/blmc_module/blmc_module.hpp>
+#include <blmc_robots/slider/slider.hpp>
 
 
 namespace blmc_robots
@@ -28,45 +29,43 @@ namespace blmc_robots
 class Finger: public BlmcModules<3>
 {
 public:
-
-    typedef blmc_drivers::MotorInterface::MeasurementIndex mi;
-    enum MotorIndexing {base, center, tip, joint_count};
+    enum JointIndexing {base, center, tip, joint_count};
 
     Finger(const std::array<std::shared_ptr<blmc_drivers::MotorInterface>, 3>& motors,
            const std::array<std::shared_ptr<blmc_drivers::AnalogSensorInterface>, 3>& sliders):
         BlmcModules<3>(motors,
                        0.02 * Eigen::Vector3d::Ones(),
                        9.0 * Eigen::Vector3d::Ones(),
-                       Eigen::Vector3d::Zero())
+                       Eigen::Vector3d::Zero()),
+        actual_sliders_(sliders,
+                        Eigen::Vector3d::Zero(),
+                        Eigen::Vector3d::Ones())
     {
-        motors_ = motors;
         sliders_ = sliders;
-
-        modules_[MotorIndexing::base]  = std::make_shared<BlmcModule> (motors_[MotorIndexing::base], 0.02, 9.0, 0.0);
-        modules_[MotorIndexing::center]  = std::make_shared<BlmcModule> (motors_[MotorIndexing::center], 0.02, 9.0, 0.0);
-        modules_[MotorIndexing::tip]  = std::make_shared<BlmcModule> (motors_[MotorIndexing::tip], 0.02, 9.0, 0.0);
     }
 
 
     const Eigen::Vector3d get_slider_positions()
     {
-        Eigen::Vector3d slider_positions;
+//        Eigen::Vector3d slider_positions;
 
-        for(size_t i = 0; i < joint_count; i++)
-        {
-            slider_positions(i) =
-                    sliders_[i]->get_measurement()->newest_element();
-        }
-        return slider_positions;
+//        for(size_t i = 0; i < joint_count; i++)
+//        {
+//            slider_positions(i) =
+//                    sliders_[i]->get_measurement()->newest_element();
+//        }
+//        return slider_positions;
+        return actual_sliders_.get_positions();
     }
 
 
 private:
-    std::array<std::shared_ptr<BlmcModule>, 3> modules_;
-
-    // we have 4 board with each possessing 2 motors and 2 sliders
-    std::array<std::shared_ptr<blmc_drivers::MotorInterface>, 3> motors_;
     std::array<std::shared_ptr<blmc_drivers::AnalogSensorInterface>, 3> sliders_;
+
+    Sliders<3> actual_sliders_;
+
+
+
 };
 
 } // namespace blmc_robots
---
+++ include/blmc_robots/slider/slider.hpp
@@ -0,0 +1,94 @@
+#pragma once
+
+#include <array>
+
+#include <Eigen/Eigen>
+#include <blmc_robots/common_header.hh>
+#include <math.h>
+#include <blmc_drivers/devices/motor.hpp>
+
+
+namespace blmc_robots
+{
+
+class Slider
+{
+public:
+
+    typedef blmc_drivers::MotorInterface::MeasurementIndex mi;
+
+
+    Slider(std::shared_ptr<blmc_drivers::AnalogSensorInterface> analog_sensor,
+           const double& min_position = 0,
+           const double& max_position = 1.0)
+    {
+        analog_sensor_ = analog_sensor;
+
+        min_position_ = min_position;
+        max_position_ = max_position;
+    }
+
+    double get_position() const
+    {
+        auto measurement_history = analog_sensor_->get_measurement();
+
+        if(measurement_history->length() == 0)
+        {
+            return std::numeric_limits<double>::quiet_NaN();
+        }
+
+        double measurement = measurement_history->newest_element();
+        return min_position_ + measurement * (max_position_ - min_position_);
+    }
+
+private:
+
+    std::shared_ptr<blmc_drivers::AnalogSensorInterface> analog_sensor_;
+
+    double min_position_;
+    double max_position_;
+};
+
+
+
+
+template <int COUNT>
+class Sliders
+{
+public:
+    typedef Eigen::Matrix<double, COUNT, 1> Vector;
+
+
+    Sliders(
+            const std::array<std::shared_ptr<blmc_drivers::AnalogSensorInterface>, COUNT>& analog_sensors,
+            const Vector& min_positions,
+            const Vector& max_positions)
+    {
+        for(size_t i = 0; i < COUNT; i++)
+        {
+            sliders_[i] = std::make_shared<Slider>(analog_sensors[i],
+                                                   min_positions[i],
+                                                   max_positions[i]);
+        }
+    }
+
+    Vector get_positions() const
+    {
+        Vector positions;
+
+        for(size_t i = 0; i < COUNT; i++)
+        {
+            positions(i) = sliders_[i]->get_position();
+        }
+        return positions;
+    }
+
+private:
+    std::array<std::shared_ptr<Slider>, COUNT> sliders_;
+
+};
+
+
+
+
+} // namespace blmc_robots
