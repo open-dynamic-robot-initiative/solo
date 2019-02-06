f32c44f2eac76c10734de602f0aa6062a3d06bc0
Steve Heim <heim@is.mpg.de>
2019-02-06 13:40:01 +0100

    stuggihop (Stuttgart hopper on the boom) files added. Comments etc. not yet clean. Demo just sends 0 torque and reads sensors

---
+++ demos/demo_stuggihop.cpp
@@ -0,0 +1,120 @@
+/**
+ * \file demo_test_bench_8_motors.cpp
+ * \brief The use of the wrapper implementing a small pid controller.
+ * \author Maximilien Naveau
+ * \date 2018
+ *
+ * This file uses the Stuggihop class in a small demo.
+ */
+
+#include <Eigen/Eigen>
+#include <deque>
+#include <numeric>
+#include <cmath>
+#include "blmc_robots/stuggihop.hpp"
+#include "real_time_tools/timer.hpp"
+
+using namespace blmc_robots;
+
+void print_vector(std::string v_name, Eigen::Ref<Eigen::VectorXd> v)
+{
+  v_name += ": [";
+  rt_printf("%s", v_name.c_str());
+  for(int i=0 ; i<v.size() ; ++i)
+  {
+    rt_printf("%f, ", v(i));
+  }
+  rt_printf("]\n");
+}
+
+static THREAD_FUNCTION_RETURN_TYPE control_loop(void* robot_void_ptr)
+{
+  Stuggihop& robot = *(static_cast<Stuggihop*>(robot_void_ptr));
+
+  // double kp = 5.0 ;
+  // double kd = 0.1 ;
+  // double max_range = 0.75 * M_PI;
+  // Vector2d desired_joint_position;
+  Vector2d desired_torque;
+
+  // Stuggihop::VectorSlider sliders;
+  // Stuggihop::VectorSlider sliders_filt;
+
+  // std::vector<std::deque<double> > sliders_filt_buffer(
+  //       robot.get_slider_positions().size());
+  // int max_filt_dim = 200;
+  // for(unsigned i=0 ; i<sliders_filt_buffer.size() ; ++i)
+  // {
+  //   sliders_filt_buffer[i].clear();
+  // }
+  size_t count = 0;
+  while(true)
+  {
+    // acquire the sensors
+    robot.acquire_sensors();
+
+    // // aquire the slider signal
+    // sliders = robot.get_slider_positions();
+    // // filter it
+    // for(unsigned i=0 ; i<sliders_filt_buffer.size() ; ++i)
+    // {
+    //   if(sliders_filt_buffer[i].size() >= max_filt_dim)
+    //   {
+    //     sliders_filt_buffer[i].pop_front();
+    //   }
+    //   sliders_filt_buffer[i].push_back(sliders(i));
+    //   sliders_filt(i) = std::accumulate(sliders_filt_buffer[i].begin(),
+    //                                     sliders_filt_buffer[i].end(), 0.0)/
+    //                     (double)sliders_filt_buffer[i].size();
+    // }
+
+    // the slider goes from 0 to 1 so we go from -0.5rad to 0.5rad
+    // for(unsigned i=0 ; i < 1 ; ++i)
+    // {
+    //   //desired_pose[i].push_back
+    //   desired_joint_position(2 * i + 0) = max_range * (sliders_filt(i) - 0.5);
+    //   desired_joint_position(2 * i + 1) = -2 * max_range * (sliders_filt(i) - 0.5);
+    // }
+
+    // we implement here a small pd control at the current level
+    desired_torque << 0.0, 0.0; 
+    // kp * (desired_joint_position - robot.get_joint_positions())
+    //                  - kd * robot.get_joint_velocities();
+
+    // // Send the current to the motor
+    robot.send_target_joint_torque(desired_torque);
+
+    // print -----------------------------------------------------------
+    real_time_tools::Timer::sleep_sec(0.001);
+
+    if ((count % 1000) == 0) {
+      print_vector("des_joint_tau  ", desired_torque);
+      print_vector("    joint_pos  ", robot.get_joint_positions());
+      print_vector("base_pos ", robot.get_base_positions());
+    }
+    ++count;
+  }//endwhile
+}// end control_loop
+
+int main(int argc, char **argv)
+{
+  real_time_tools::RealTimeThread thread;
+
+  Stuggihop robot;
+
+  robot.initialize();
+
+  rt_printf("controller is set up \n");
+
+  real_time_tools::create_realtime_thread(thread, &control_loop, &robot);
+
+  rt_printf("control loop started \n");
+
+  while(true)
+  {
+      real_time_tools::Timer::sleep_sec(0.01);
+  }
+
+  return 0;
+}
+
---
+++ src/stuggihop.cpp
@@ -0,0 +1,219 @@
+#include <math.h>
+#include "blmc_robots/stuggihop.hpp"
+
+namespace blmc_robots{
+
+Stuggihop::Stuggihop()
+{
+  /**
+    * Motor data
+    */
+  motor_positions_.setZero();
+  motor_velocities_.setZero();
+  motor_currents_.setZero();
+  motor_torques_.setZero();
+  motor_inertias_.setZero();
+  motor_encoder_indexes_.setZero();
+  motor_target_currents_.setZero();
+  motor_target_torques_.setZero();
+  motor_torque_constants_.setZero();
+  target_motor_current_tmp_.setZero();
+
+  /**
+    * Joint data
+    */
+  joint_positions_.setZero();
+  joint_velocities_.setZero();
+  joint_torques_.setZero();
+  joint_target_torques_.setZero();
+  joint_gear_ratios_.setZero();
+  joint_zero_positions_.setZero();
+
+  /**
+    * Additional data
+    */
+  // slider_positions_.setZero();
+  motor_max_current_.setZero();
+
+  /**
+    * Boom parameters
+    */
+  boom_gear_ratios_ << 2.88, 1; // horizontal and vertical
+  boom_encoder_index_.setZero();
+  boom_zero_positions_.setZero();
+  boom_zero_positions_.setZero();
+  boom_length_ << 2; // meters
+  base_positions_.setZero();
+  base_velocities_.setZero();
+
+  /**
+    * Setup some known data
+    */
+
+  // for now this value is very small but it is currently for debug mode
+  motor_max_current_.fill(4.5);
+  motor_torque_constants_.fill(0.025);
+  motor_inertias_.fill(0.045);
+  joint_gear_ratios_.fill(9.0);
+  joint_max_torque_ = motor_max_current_.array() *
+                      motor_torque_constants_.array() *
+                      joint_gear_ratios_.array();
+}
+
+
+void Stuggihop::initialize()
+{
+  // initialize the communication with the can cards
+  // for(unsigned i=0 ; i< can_buses_.size() ; ++i)
+  for(unsigned i=0 ; i< 2 ; ++i)
+  {
+    std::ostringstream oss;
+    oss << "can" << i; // hard-coding to "can0" and "can1"
+    can_buses_[i] = std::make_shared<blmc_drivers::CanBus>(oss.str());
+    can_motor_boards_[i] =
+        std::make_shared<blmc_drivers::CanBusMotorBoard>(can_buses_[i]);
+  }
+
+  // sliders_[0] =
+  //     std::make_shared<blmc_drivers::AnalogSensor>(can_motor_boards_[0], 1);
+  // contact_sensors_[0] =
+  //     std::make_shared<blmc_drivers::AnalogSensor>(can_motor_boards_[1], 0);
+  // height_sensors_[0] =
+  //     std::make_shared<blmc_drivers::AnalogSensor>(can_motor_boards_[1], 1);
+
+  // can 0
+  // MOTOR_HFE
+  motors_[0] = std::make_shared<blmc_drivers::SafeMotor> (
+                 can_motor_boards_[0], 1, motor_max_current_[0]);
+  // MOTOR_KFE
+  motors_[1] = std::make_shared<blmc_drivers::SafeMotor> (
+                 can_motor_boards_[0], 0, motor_max_current_[1]);
+
+  // can1
+  // Boom encoders: horizontal and vertical
+  // 
+  boom_encoders_[0] = std::make_shared<blmc_drivers::SafeMotor> (
+                 can_motor_boards_[1], 0, motor_max_current_[0]);
+  // MOTOR_KFE
+  boom_encoders_[1] = std::make_shared<blmc_drivers::SafeMotor> (
+                 can_motor_boards_[1], 1, motor_max_current_[1]);
+
+  // Call the method to sync the max current with the max torques.
+  set_max_current(motor_max_current_);
+
+  // Wait to make sure there is a first package when acquire_sensors() later.
+  real_time_tools::Timer::sleep_sec(0.5);
+}
+
+void Stuggihop::acquire_sensors()
+{
+  /**
+    * Motor data
+    */
+  for (unsigned i=0 ; i<motors_.size() ; ++i)
+  {
+    // acquire the motors positions
+    motor_positions_(i) =
+        motors_[i]->get_measurement(mi::position)->newest_element()
+        - joint_zero_positions_(i) * joint_gear_ratios_(i) ;
+    // acquire the motors velocities
+    motor_velocities_(i) =
+        motors_[i]->get_measurement(mi::velocity)->newest_element();
+    // acquire the motors current
+    motor_currents_(i) =
+        motors_[i]->get_measurement(mi::current)->newest_element();
+    // acquire the last sent current sent
+    motor_target_currents_(i) =
+        motors_[i]->get_sent_current_target()->newest_element();
+    // acquire the encoder indexes
+    motor_encoder_indexes_(i) =
+        motors_[i]->get_measurement(mi::encoder_index)->length() > 0 ?
+          motors_[i]->get_measurement(mi::encoder_index)->newest_element():
+          std::nan("");
+  }
+  // acquire the actual motor torques
+  motor_torques_ = motor_currents_.array() * motor_torque_constants_.array();
+  // acquire the last sent motor torques
+  motor_target_torques_ = motor_target_currents_.array() *
+                          motor_torque_constants_.array();
+
+  /**
+    * Joint data
+    */
+  // acquire the joint position
+  joint_positions_ = motor_positions_.array() / joint_gear_ratios_.array();
+  // acquire the joint velocities
+  joint_velocities_ = motor_velocities_.array() / joint_gear_ratios_.array();
+  // acquire the joint torques
+  joint_torques_ = motor_torques_.array() * joint_gear_ratios_.array();
+  // acquire the target joint torques
+  joint_target_torques_ = motor_target_torques_.array() *
+                          joint_gear_ratios_.array();
+  joint_encoder_index_ = motor_encoder_indexes_.array() /
+                          joint_gear_ratios_.array();
+
+  /**
+    * Boom data
+    */
+  for (unsigned i=0 ; i<boom_encoders_.size() ; ++i)
+  {
+    // acquire the boom positions
+    boom_positions_(i) =
+        boom_encoders_[i]->get_measurement(mi::position)->newest_element()
+        - boom_zero_positions_(i);
+    // acquire the boom velocities
+    boom_velocities_(i) =
+        boom_encoders_[i]->get_measurement(mi::velocity)->newest_element();    
+    boom_encoder_index_(i) =
+        boom_encoders_[i]->get_measurement(mi::encoder_index)->length() > 0 ?
+          boom_encoders_[i]->get_measurement(mi::encoder_index)->newest_element():
+          std::nan("");
+
+  }
+  /** 
+  * Floating Base data
+  */
+  // for whatever reason, this does not work all on one line
+  // boom_positions_.array() * boom_gear_ratios_.array()*boom_length_.array();
+  base_positions_ = boom_positions_.array() * boom_gear_ratios_.array();
+  base_positions_ *= boom_length_;
+  base_velocities_ = boom_velocities_.array() * boom_gear_ratios_.array();
+  base_velocities_ *= boom_length_;
+
+  /**
+    * Additional data
+    */
+  // // acquire the slider positions
+  // for (unsigned i=0 ; i < slider_positions_.size() ; ++i)
+  // {
+  //   // acquire the slider
+  //   slider_positions_(i) = sliders_[i]->get_measurement()->newest_element();
+  // }
+}
+
+void Stuggihop::send_target_motor_current(
+    const Eigen::Ref<Vector2d> target_motor_current)
+{
+  // set up the target current
+  for(unsigned i=0 ; i<motors_.size() ; ++i)
+  {
+    motors_[i]->set_current_target(target_motor_current(i));
+  }
+
+  // actually send the torques to the robot
+  for(unsigned i=0 ; i<motors_.size() ; ++i)
+  {
+    motors_[i]->send_if_input_changed();
+  }
+}
+
+void Stuggihop::send_target_joint_torque(
+    const Eigen::Ref<Vector2d> target_joint_torque)
+{
+  target_motor_current_tmp_ = target_joint_torque.array() /
+                              joint_gear_ratios_.array() /
+                              motor_torque_constants_.array();
+  send_target_motor_current(target_motor_current_tmp_);
+}
+
+} // namespace blmc_robots
