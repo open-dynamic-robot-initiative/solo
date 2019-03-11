d55cd45a4cac85d0cf2ff2ea3da9552c77fc5d34
Waleed <waleed.gondal@tue.mpg.de>
2019-03-11 11:48:10 +0100

    Reverted the last 2 commits from disentanglement_ros package

--- demos/demo_disentanglement_platform.cpp
+++ demos/demo_disentanglement_platform.cpp
@@ -81,8 +81,8 @@ private:
   void loop()
   {
 
-      double kp = 1.2;
-      double kd = 0.04;
+      double kp = 0.5;
+      double kd = 0.0025;
 
       real_time_tools::Spinner spinner;
       double interval = 0.001;
@@ -90,7 +90,7 @@ private:
       size_t count = 0;
 
       double time = 0;
-      double frequency = 0.1;
+      double frequency = 0.2;
       while(true)
       {
           double desired_angle = (sin(time * frequency * 2 * M_PI)) * 0.5 * M_PI;
--- include/blmc_robots/stuggihop.hpp
+++ include/blmc_robots/stuggihop.hpp
@@ -3,7 +3,7 @@
 
 #include <blmc_robots/common_header.hpp>
 #include <blmc_robots/blmc_joint_module.hpp>
-// #include <blmc_robots/slider.hpp>
+#include <blmc_robots/slider.hpp>
 
 namespace blmc_robots{
 
