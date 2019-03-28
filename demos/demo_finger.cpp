12fa2b105d96c33cc7aedbf56d87152037204026
manuel <manuel.wuthrich@gmail.com>
2019-03-28 13:02:05 +0100

    changed finger class and demo finger such that setting up of the robot etc is done inside the finger class

--- demos/demo_finger.cpp
+++ demos/demo_finger.cpp
@@ -26,7 +26,8 @@ static THREAD_FUNCTION_RETURN_TYPE control_loop(void* robot_void_ptr)
   while(true)
   {
     // the slider goes from 0 to 1 so we go from -0.5rad to 0.5rad
-    Eigen::Vector3d desired_angles = (robot.get_slider_positions().array() - 0.5) * 2 * M_PI;
+    Eigen::Vector3d desired_angles  =
+            (robot.get_slider_positions().array() - 0.5) * 2 * M_PI;
 
     // we implement here a small pd control at the current level
     Eigen::Vector3d desired_torque = kp * (desired_angles - robot.get_angles()) -
@@ -36,10 +37,9 @@ static THREAD_FUNCTION_RETURN_TYPE control_loop(void* robot_void_ptr)
     robot.set_torques(desired_torque);
     robot.send_torques();
 
-
-    // print -------------------------------------------------------------------
     spinner.spin();
 
+    // print -------------------------------------------------------------------
     if ((count % 1000) == 0)
     {
       rt_printf("sending currents: [");
@@ -65,35 +65,8 @@ static THREAD_FUNCTION_RETURN_TYPE control_loop(void* robot_void_ptr)
 
 int main(int argc, char **argv)
 {
-    // initialize the communication with the can cards
-    auto can_bus_0 = std::make_shared<blmc_drivers::CanBus>("can0");
-    auto can_bus_1 = std::make_shared<blmc_drivers::CanBus>("can1");
-
-    // get all informatino about the control cards
-    auto board_0 = std::make_shared<blmc_drivers::CanBusMotorBoard>(can_bus_0);
-    auto board_1 = std::make_shared<blmc_drivers::CanBusMotorBoard>(can_bus_1);
-
-    // get the drivers for the motors and the sliders
-    // two individual motors on individual leg style mounting on the left of the
-    // table
-    std::array<std::shared_ptr<blmc_drivers::MotorInterface>, 3> motors;
-    double max_motor_velocity = 20;
-    motors[0]  = std::make_shared<blmc_drivers::SafeMotor>(board_0, 0, 2.0,
-        1000, max_motor_velocity);
-    motors[1]  = std::make_shared<blmc_drivers::SafeMotor>(board_0, 1, 2.0,
-        1000, max_motor_velocity);
-    motors[2]  = std::make_shared<blmc_drivers::SafeMotor>(board_1, 0, 2.0,
-        1000, max_motor_velocity);
-
-    std::array<std::shared_ptr<blmc_drivers::AnalogSensorInterface>, 3> sliders;
-    sliders[0] = std::make_shared<blmc_drivers::AnalogSensor>(board_0, 0);
-    sliders[1] = std::make_shared<blmc_drivers::AnalogSensor>(board_0, 1);
-    sliders[2] = std::make_shared<blmc_drivers::AnalogSensor>(board_1, 0);
-
-    Finger finger(motors, sliders);
-
-    board_0->wait_until_ready();
-    board_1->wait_until_ready();
+    Finger finger;
+    rt_printf("done creating finger \n");
 
     real_time_tools::block_memory();
     real_time_tools::RealTimeThread thread;
--- include/blmc_robots/finger.hpp
+++ include/blmc_robots/finger.hpp
@@ -14,6 +14,8 @@
 #include <Eigen/Eigen>
 #include <blmc_robots/common_header.hpp>
 #include <math.h>
+#include <tuple>
+
 
 #include <blmc_robots/blmc_joint_module.hpp>
 #include <blmc_robots/slider.hpp>
@@ -31,25 +33,81 @@ class Finger: public BlmcJointModules<3>
 public:
     enum JointIndexing {base, center, tip, joint_count};
 
-    Finger(const std::array<std::shared_ptr<blmc_drivers::MotorInterface>, 3>& motors,
-           const std::array<std::shared_ptr<blmc_drivers::AnalogSensorInterface>, 3>& sliders):
+    typedef std::array<std::shared_ptr<blmc_drivers::MotorInterface>, 3> Motors;
+    typedef std::array<std::shared_ptr<blmc_drivers::AnalogSensorInterface>, 3>
+    AnalogSensors;
+
+    Finger(): Finger(create_motors_and_analog_sensors()){}
+
+    Finger(const std::tuple<Motors, AnalogSensors>& motors_and_analog_sensors):
+        Finger(std::get<0>(motors_and_analog_sensors),
+               std::get<1>(motors_and_analog_sensors)) {}
+
+
+    Finger(const Motors& motors, const AnalogSensors& analog_sensors):
         BlmcJointModules<3>(motors,
-                       0.02 * Eigen::Vector3d::Ones(),
-                       9.0 * Eigen::Vector3d::Ones(),
-                       Eigen::Vector3d::Zero()),
-        sliders_(sliders,
+                            0.02 * Eigen::Vector3d::Ones(),
+                            9.0 * Eigen::Vector3d::Ones(),
+                            Eigen::Vector3d::Zero()),
+        sliders_(analog_sensors,
                  Eigen::Vector3d::Zero(),
                  Eigen::Vector3d::Ones()) {}
 
 
+public:
     const Eigen::Vector3d get_slider_positions()
     {
         return sliders_.get_positions();
     }
 
-private:
 
+
+
+private:
     Sliders<3> sliders_;
+
+
+
+    std::tuple<Motors, AnalogSensors> create_motors_and_analog_sensors()
+    {
+        // setup can buses -----------------------------------------------------
+        std::array<std::shared_ptr<blmc_drivers::CanBus>, 2> can_buses;
+        can_buses[0] = std::make_shared<blmc_drivers::CanBus>("can0");
+        can_buses[1] = std::make_shared<blmc_drivers::CanBus>("can1");
+
+        // set up motor boards -------------------------------------------------
+        std::array<std::shared_ptr<blmc_drivers::CanBusMotorBoard>, 2>
+                motor_boards;
+        motor_boards[0] =
+                std::make_shared<blmc_drivers::CanBusMotorBoard>(can_buses[0]);
+        motor_boards[1] =
+                std::make_shared<blmc_drivers::CanBusMotorBoard>(can_buses[1]);
+        motor_boards[0]->wait_until_ready();
+        motor_boards[1]->wait_until_ready();
+
+
+        // set up motors -------------------------------------------------------
+        Motors motors;
+        double max_motor_velocity = 20;
+        motors[0]  = std::make_shared<blmc_drivers::SafeMotor>(motor_boards[0],
+                0, 2.0, 1000, max_motor_velocity);
+        motors[1]  = std::make_shared<blmc_drivers::SafeMotor>(motor_boards[0],
+                1, 2.0, 1000, max_motor_velocity);
+        motors[2]  = std::make_shared<blmc_drivers::SafeMotor>(motor_boards[1],
+                0, 2.0, 1000, max_motor_velocity);
+
+        // set up sliders ------------------------------------------------------
+        AnalogSensors analog_sensors;
+        analog_sensors[0] = std::make_shared<blmc_drivers::AnalogSensor>(
+                    motor_boards[0], 0);
+        analog_sensors[1] = std::make_shared<blmc_drivers::AnalogSensor>(
+                    motor_boards[0], 1);
+        analog_sensors[2] = std::make_shared<blmc_drivers::AnalogSensor>(
+                    motor_boards[1], 0);
+
+        return std::make_tuple(motors, analog_sensors);
+    }
+
 };
 
 } // namespace blmc_robots
