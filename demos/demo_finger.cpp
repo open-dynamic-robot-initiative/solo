b352605c85733a54cd622e70fa42c466b79b3be5
manuel <manuel.wuthrich@gmail.com>
2019-04-13 23:34:58 +0200

    preparing to move to split finger into the actual robot and the sliders, everything still working

--- demos/demo_finger.cpp
+++ demos/demo_finger.cpp
@@ -9,14 +9,16 @@
 
 #include "real_time_tools/spinner.hpp"
 #include "blmc_robots/real_finger.hpp"
+#include "robot_interfaces/finger.hpp"
 #include <iostream>
 
 using namespace blmc_robots;
+using namespace robot_interfaces;
 
 
-static THREAD_FUNCTION_RETURN_TYPE control_loop(void* robot_void_ptr)
+static THREAD_FUNCTION_RETURN_TYPE control_loop(void* finger_void_ptr)
 {
-    RealFinger& robot = *(static_cast<RealFinger*>(robot_void_ptr));
+    RealFinger& finger = *(static_cast<RealFinger*>(finger_void_ptr));
 
     double kp = 0.2;
     double kd = 0.0025;
@@ -28,15 +30,15 @@ static THREAD_FUNCTION_RETURN_TYPE control_loop(void* robot_void_ptr)
     {
         // the slider goes from 0 to 1 so we go from -0.5rad to 0.5rad
         Eigen::Vector3d desired_angles  =
-                (robot.get_slider_positions().array() - 0.5) * 2 * M_PI;
+                (finger.get_slider_positions().array() - 0.5) * 2 * M_PI;
 
         // we implement here a small pd control at the current level
-        Eigen::Vector3d desired_torque = kp * (desired_angles - robot.get_angles()) -
-                kd * robot.get_angular_velocities();
+        Eigen::Vector3d desired_torque = kp * (desired_angles - finger.get_angles()) -
+                kd * finger.get_angular_velocities();
 
         // Send the current to the motor
-        robot.set_torques(desired_torque);
-        robot.send_torques();
+        finger.set_torques(desired_torque);
+        finger.send_torques();
 
         spinner.spin();
 
@@ -44,7 +46,7 @@ static THREAD_FUNCTION_RETURN_TYPE control_loop(void* robot_void_ptr)
         if ((count % 1000) == 0)
         {
             std::cout << "desired_torque: " << desired_torque.transpose() << std::endl;
-            std::cout << "angles: " << robot.get_angles().transpose() << std::endl;
+            std::cout << "angles: " << finger.get_angles().transpose() << std::endl;
         }
         ++count;
     }//endwhile
@@ -52,12 +54,12 @@ static THREAD_FUNCTION_RETURN_TYPE control_loop(void* robot_void_ptr)
 
 int main(int argc, char **argv)
 {
-    RealFinger finger("can0", "can1");
+    auto finger = std::make_shared<RealFinger>(RealFinger("can0", "can1"));
     rt_printf("done creating finger \n");
 
     real_time_tools::block_memory();
     real_time_tools::RealTimeThread thread;
-    real_time_tools::create_realtime_thread(thread, &control_loop, &finger);
+    real_time_tools::create_realtime_thread(thread, &control_loop, finger.get());
 
     rt_printf("control loop started \n");
 
