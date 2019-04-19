d4628d7696716440ba6c87e45b8f7db6d20e15e2
manuel <manuel.wuthrich@gmail.com>
2019-04-19 13:04:14 +0200

    started implementing new disentanglement_platform

--- demos/demo_disentanglement_platform.cpp
+++ demos/demo_disentanglement_platform.cpp
@@ -1,213 +1,73 @@
 /**
- * \file demo_test_bench_8_motors.cpp
- * \brief The use of the wrapper implementing a small pid controller.
- * \author Maximilien Naveau
- * \date 2018
+ * \file demo_disentanglement_platform.cpp
+ * \brief a demo illustrating how to use the RealDisentanglementPlatform class
+ * \author Manuel Wuthrich
+ * \date 2019
  *
- * This file uses the RealFinger class in a small demo.
+ * This file uses the RealDisentanglementPlatform class in a small demo.
  */
 
+#include <iostream>
+#include <tuple>
+#include <Eigen/Core>
+
 #include "real_time_tools/spinner.hpp"
-#include "blmc_robots/real_finger.hpp"
-
 #include "blmc_robots/disentanglement_platform.hpp"
+#include "real_time_tools/realtime_thread_creation.hpp"
 
-#include <math.h>
 
 using namespace blmc_robots;
 
 
-class Controller
+
+static THREAD_FUNCTION_RETURN_TYPE control_loop(void* robot_ptr)
 {
-public:
-  /**
-   * @brief Construct a new PDController object.
-   *
-   * @param motor_slider_pairs
-   */
-  Controller(std::shared_ptr<DisentanglementPlatform> disentanglement_platform):
-      disentanglement_platform_(disentanglement_platform)
-  {
-    stop_loop=false;
-  }
+    // cast input arguments to the right format --------------------------------
+    std::shared_ptr<RealDisentanglementPlatform>& robot =
+            *(static_cast<std::shared_ptr<RealDisentanglementPlatform>*>(robot_ptr));
 
-  /**
-   * @brief Destroy the PDController object
-   */
-  ~Controller()
-  {
-    stop_loop=true;
-    real_time_tools::join_thread(rt_thread_);
-  }
+    // position controller -----------------------------------------------------
+    double kp = 0.2;
+    double kd = 0.0025;
 
-  /**
-   * @brief This method is a helper to start the thread loop.
-   */
-  void start_loop()
-  {
-    real_time_tools::create_realtime_thread(
-          rt_thread_, &Controller::loop, this);
-  }
+    real_time_tools::Spinner spinner;
+    spinner.set_period(0.001);
+    size_t count = 0;
+    while(true)
+    {
+        Eigen::Vector3d desired_torque = 0.5 * 0.02 * 9.0 *
+                Eigen::Vector3d(9.79*0, 1*0, 1);
 
-private:
-
-  std::shared_ptr<DisentanglementPlatform> disentanglement_platform_;
-
-  /**
-   * @brief This is a pair of motor and sliders so that we associate one with
-   * the other.
-   */
-  /**
-   * @brief This is the real time thread object.
-   */
-  real_time_tools::RealTimeThread rt_thread_;
-
-  /**
-     * @brief this function is just a wrapper around the actual loop function,
-     * such that it can be spawned as a posix thread.
-     */
-  static THREAD_FUNCTION_RETURN_TYPE loop(void* instance_pointer)
-  {
-    ((Controller*)(instance_pointer))->loop();
-  }
-
-  /**
-     * @brief this is a simple control loop which runs at a kilohertz.
-     *
-     * it reads the measurement from the analog sensor, in this case the
-     * slider. then it scales it and sends it as the current target to
-     * the motor.
-     */
-  void loop()
-  {
-
-      double kp = 0.5;
-      double kd = 0.0025;
-
-      real_time_tools::Spinner spinner;
-      double interval = 0.001;
-      spinner.set_period(interval);
-      size_t count = 0;
-
-      double time = 0;
-      double frequency = 0.2;
-      while(true)
-      {
-          double desired_angle = (sin(time * frequency * 2 * M_PI)) * 0.5 * M_PI;
-
-
-          time += interval;
-
-        // the slider goes from 0 to 1 so we go from -0.5rad to 0.5rad
-        Eigen::Vector2d desired_angles = (disentanglement_platform_->get_slider_positions().array() - 0.5) * 2 * M_PI;
-
-        desired_angles[0] = desired_angle;
-        desired_angles[1] = desired_angle;
-
-
-        // we implement here a small pd control at the current level
-        Eigen::Vector2d desired_torque = kp * (desired_angles - disentanglement_platform_->get_measured_angles()) -
-                          kd * disentanglement_platform_->get_measured_velocities();
-
-
-        // Send the current to the motor
-        disentanglement_platform_->set_torques(desired_torque);
-        disentanglement_platform_->send_torques();
-
-
-        // print -------------------------------------------------------------------
+        robot->apply_torques(desired_torque);
         spinner.spin();
 
+        // print ---------------------------------------------------------------
         if ((count % 1000) == 0)
         {
-          rt_printf("sending torques: [");
-          for(int i=0 ; i<desired_torque.size() ; ++i)
-          {
-            rt_printf("%f, ", desired_torque(i));
-          }
-          rt_printf("]\n");
-
-          rt_printf("desired angles: [");
-          for(int i=0 ; i < desired_angles.size() ; ++i)
-          {
-            rt_printf("%f, ", desired_angles(i));
-          }
-          rt_printf("]\n");
-
-
-
-          Eigen::Vector2d angles = disentanglement_platform_->get_measured_angles();
-
-          rt_printf("positions: [");
-          for(int i=0 ; i < angles.size() ; ++i)
-          {
-            rt_printf("%f, ", angles(i));
-          }
-          rt_printf("]\n");
+            std::cout << "desired_torque: "
+                      << desired_torque.transpose() << std::endl;
+            std::cout << "angles: "
+                      << robot->get_measured_angles().transpose() << std::endl;
         }
         ++count;
-      }//endwhile
-
-  }
-
-  /**
-   * @brief managing the stopping of the loop
-   */
-  bool stop_loop;
-
-}; // end class PDController definition
-
-
+    }
+}
 
 int main(int argc, char **argv)
 {
+    // set up motor boards -----------------------------------------------------
+    auto motor_boards =
+            RealDisentanglementPlatform::create_motor_boards("can0", "can1");
+
+    // set up disentanglement_platform -----------------------------------------------------------
+    auto robot = std::make_shared<RealDisentanglementPlatform>(motor_boards);
+
+    // start real-time control loop --------------------------------------------
     real_time_tools::RealTimeThread thread;
-    osi::initialize_realtime_printing();
-
-    // slave finger ------------------------------------------------------------
-    std::shared_ptr<DisentanglementPlatform> disentanglement_platform;
-    {
-        // initialize the communication with the can cards
-        auto can_bus = std::make_shared<blmc_drivers::CanBus>("can0");
-
-        // get all informatino about the control cards
-        auto board = std::make_shared<blmc_drivers::CanBusMotorBoard>(can_bus);
-
-        // get the drivers for the motors and the sliders
-        // two individual motors on individual leg style mounting on the left of the
-        // table
-        std::array<std::shared_ptr<blmc_drivers::MotorInterface>, 2> motors;
-        motors[0]  = std::make_shared<blmc_drivers::SafeMotor>   (board, 0, 1.0);
-        motors[1]  = std::make_shared<blmc_drivers::SafeMotor>   (board, 1, 1.0);
-
-        std::array<std::shared_ptr<blmc_drivers::AnalogSensorInterface>, 2> sliders;
-        sliders[0] = std::make_shared<blmc_drivers::AnalogSensor>(board, 0);
-        sliders[1] = std::make_shared<blmc_drivers::AnalogSensor>(board, 1);
-
-        disentanglement_platform = std::make_shared<DisentanglementPlatform>(motors, sliders);
-    }
-
-
-
-    Controller controller(disentanglement_platform);
-
-
-
-
-    real_time_tools::Timer::sleep_sec(5.);
-
-    rt_printf("controller is set up \n");
-
-    real_time_tools::block_memory();
-    controller.start_loop();
-//    real_time_tools::create_realtime_thread(thread, &control_loop, &(*disentanglement_platform));
-
+    real_time_tools::create_realtime_thread(thread,
+                                            &control_loop,
+                                            &robot);
     rt_printf("control loop started \n");
-
-    while(true)
-    {
-        real_time_tools::Timer::sleep_sec(0.01);
-    }
-
+    real_time_tools::join_thread(thread);
     return 0;
 }
--- include/blmc_robots/disentanglement_platform.hpp
+++ include/blmc_robots/disentanglement_platform.hpp
@@ -1,10 +1,10 @@
 /**
  * \file test_bench_8_motors.hh
- * \brief The hardware wrapper of the DisentanglementPlatform
+ * \brief The hardware wrapper of the OldRealDisentanglementPlatform
  * \author Manuel Wuthrich
  * \date 2018
  *
- * This file declares the DisentanglementPlatform class which defines the test
+ * This file declares the OldRealDisentanglementPlatform class which defines the test
  * bench with 8 motors.
  */
 
@@ -19,19 +19,20 @@
 #include <blmc_robots/slider.hpp>
 
 
+
 namespace blmc_robots
 {
 
 /**
- * @brief The DisentanglementPlatform class implements the control of the test bench
+ * @brief The OldRealDisentanglementPlatform class implements the control of the test bench
  * containing 8 motors and 8 sliders using the blmc drivers.
  */
-class DisentanglementPlatform: public BlmcJointModules<2>
+class OldDisentanglementPlatform: public BlmcJointModules<2>
 {
 public:
     enum JointIndexing {base, tip, joint_count};
 
-    DisentanglementPlatform(const std::array<std::shared_ptr<blmc_drivers::MotorInterface>, 2>& motors,
+    OldDisentanglementPlatform(const std::array<std::shared_ptr<blmc_drivers::MotorInterface>, 2>& motors,
            const std::array<std::shared_ptr<blmc_drivers::AnalogSensorInterface>, 2>& sliders):
         BlmcJointModules<2>(motors,
                 0.02 * Eigen::Vector2d::Ones(),
@@ -49,6 +50,179 @@ public:
 private:
 
     Sliders<2> sliders_;
+};
+
+
+
+
+
+class RealDisentanglementPlatform: public BlmcJointModules<3>
+{
+public:
+    typedef Eigen::Vector3d Vector;
+
+
+    typedef std::array<std::shared_ptr<blmc_drivers::MotorInterface>, 3> Motors;
+    typedef std::array<std::shared_ptr<blmc_drivers::CanBusMotorBoard>, 2>
+    MotorBoards;
+
+    RealDisentanglementPlatform(const MotorBoards& motor_boards):
+        RealDisentanglementPlatform(create_motors(motor_boards))
+    {
+        motor_boards_ = motor_boards;
+
+
+        pause_motors();
+
+
+
+
+        /// \todo calibrate etc
+//        calibrate();
+//        set_angle_limits(Vector::Zero(),
+//                         Vector(176, 170, 326) / 180.0 * M_PI);
+//        set_max_velocities(Vector::Ones() * std::numeric_limits<double>::quiet_NaN());
+    }
+
+private:
+    RealDisentanglementPlatform(const Motors& motors):
+        BlmcJointModules<3>(motors,
+                0.02 * Vector::Ones(),
+                Vector(9.79 * 9, 9, 9),
+                Vector::Zero()) {}
+
+public:
+    void apply_torques(const Vector& desired_torques)
+    {
+        BlmcJointModules<3>::set_torques(desired_torques);
+        BlmcJointModules<3>::send_torques();
+    }
+
+
+    Vector get_measured_torques() const
+    {
+        return BlmcJointModules<3>::get_measured_torques();
+    }
+
+    Vector get_measured_angles() const
+    {
+        return BlmcJointModules<3>::get_measured_angles();
+    }
+
+    Vector get_measured_velocities() const
+    {
+        return BlmcJointModules<3>::get_measured_velocities();
+    }
+
+    void pause_motors()
+    {
+        motor_boards_[0]->pause_motors();
+        motor_boards_[1]->pause_motors();
+    }
+
+    void wait_for_execution() const
+    {
+        /// \todo: this needs to be filled in
+    }
+
+
+    /// \todo: this is identical to the finger robot and could be taken out of
+    /// class
+    static MotorBoards
+    create_motor_boards(const std::string& can_0, const std::string& can_1)
+    {
+        // setup can buses -----------------------------------------------------
+        std::array<std::shared_ptr<blmc_drivers::CanBus>, 2> can_buses;
+        can_buses[0] = std::make_shared<blmc_drivers::CanBus>(can_0);
+        can_buses[1] = std::make_shared<blmc_drivers::CanBus>(can_1);
+
+        // set up motor boards -------------------------------------------------
+        MotorBoards motor_boards;
+        motor_boards[0] =
+                std::make_shared<blmc_drivers::CanBusMotorBoard>(can_buses[0]);
+        motor_boards[1] =
+                std::make_shared<blmc_drivers::CanBusMotorBoard>(can_buses[1]);
+
+        motor_boards[0]->wait_until_ready();
+        motor_boards[1]->wait_until_ready();
+
+        return motor_boards;
+    }
+
+
+private:
+    MotorBoards motor_boards_;
+
+    /// \todo: this is identical to the finger robot and could be taken out of
+    /// class
+    static Motors
+    create_motors(const MotorBoards& motor_boards)
+    {
+        // set up motors -------------------------------------------------------
+        Motors motors;
+        motors[0]  = std::make_shared<blmc_drivers::Motor>(motor_boards[0], 0);
+        motors[1]  = std::make_shared<blmc_drivers::Motor>(motor_boards[0], 1);
+        motors[2]  = std::make_shared<blmc_drivers::Motor>(motor_boards[1], 0);
+
+        return motors;
+    }
+
+    /**
+     * @brief this is an initial calibration procedure executed before the actual
+     * control loop to have angle readings in an absolute coordinate frame.
+     *
+     * The finger reaches its joint limits by applying a constant negative torque
+     * simultaneously in all the joints. Be aware that this procedure assumes safe
+     * motors where the maximum velocities are constrained.
+     *
+     * The calibration procedure is finished once the joint velocities are zero
+     * according to a moving average estimation of them.
+     */
+//    void calibrate()
+//    {
+//        /// \todo: this relies on the safety check in the motor right now,
+//        /// which is maybe not the greatest idea. without the velocity and
+//        /// torque limitation in the motor this would be very unsafe
+//        real_time_tools::Spinner spinner;
+//        spinner.set_period(0.001);
+//        std::vector<Vector> running_velocities(1000);
+//        int running_index = 0;
+//        Vector sum = Vector::Zero();
+//        while(running_index < 3000 || (sum.maxCoeff() / 1000.0 > 0.001))
+//        {
+//            Vector torques = -1 * get_max_torques();
+//            constrain_and_apply_torques(torques);
+//            Vector velocities = get_measured_velocities();
+//            if (running_index >= 1000)
+//                sum = sum - running_velocities[running_index % 1000];
+//            running_velocities[running_index % 1000] = velocities;
+//            sum = sum + velocities;
+//            running_index++;
+//            spinner.spin();
+//        }
+//        int count = 0;
+//        int linearly_decrease_time_steps = 1000;
+//        int zero_torque_time_steps = 500;
+//        while(count < linearly_decrease_time_steps)
+//        {
+//            Vector torques = ((linearly_decrease_time_steps -
+//                    count + 0.0) / linearly_decrease_time_steps) *
+//                    get_max_torques() * -1;
+//            constrain_and_apply_torques(torques);
+//            count++;
+//            spinner.spin();
+//        }
+//        count = 0;
+//        while(count < zero_torque_time_steps)
+//        {
+//            Vector torques = Vector::Zero();
+//            constrain_and_apply_torques(torques);
+//            count++;
+//            spinner.spin();
+//        }
+//        Vector angle_offsets = get_measured_angles();
+//        set_zero_angles(angle_offsets);
+//    }
 };
 
 } // namespace blmc_robots
