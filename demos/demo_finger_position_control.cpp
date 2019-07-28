69ee2a3ed58e233ca35f9c0095e5bb1ab46699f5
manuel <manuel.wuthrich@tuebingen.mpg.de>
2019-07-28 11:38:58 +0200

    adapted demo_finger_position_control to use new interface, everything seems to be working smoothly

--- demos/demo_finger_position_control.cpp
+++ demos/demo_finger_position_control.cpp
@@ -38,31 +38,24 @@ static THREAD_FUNCTION_RETURN_TYPE control_loop(
     double kp = 0.2;
     double kd = 0.0025;
 
-    real_time_tools::Spinner spinner;
-    spinner.set_period(0.001);
-    size_t count = 0;
-    while(true)
+    NewFinger::Action desired_torque = NewFinger::Action::Zero();
+    NewFinger::Data data = finger->step(desired_torque);
+    for(size_t count = 0; true; count++)
     {
-        Finger::Observation observation = finger->get_latest_observation();
-        Eigen::Vector3d desired_torque =
-                kp * (sliders->get_positions()- observation.angle) -
-                kd * observation.velocity;
+        desired_torque =
+                kp * (sliders->get_positions()- data.observation.angle) -
+                kd * data.observation.velocity;
 
-        finger->constrain_and_apply_torques(desired_torque);
-        // usleep(1000);
-        real_time_tools::Timer::sleep_ms(1);
-
-        // spinner.spin();
+        data = finger->step(desired_torque);
 
         // print ---------------------------------------------------------------
         if ((count % 1000) == 0)
         {
             std::cout << "desired_torque: "
-                      << desired_torque.transpose() << std::endl;
+                      << data.desired_action << std::endl;
             std::cout << "angles: "
-                      << finger->get_measured_angles().transpose() << std::endl;
+                      << data.observation.angle << std::endl;
         }
-        ++count;
     }
 }
 
