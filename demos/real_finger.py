8caf14eeb24ff71d8055fec5196c0b02871bbef0
manuel <manuel.wuthrich@tuebingen.mpg.de>
2019-07-30 22:20:52 +0200

    created little python demo for finger

--- demos/real_finger.py
+++ demos/real_finger.py
@@ -1,18 +1,28 @@
 import time
 import numpy as np
 
+import threading
 import ipdb
 from py_real_finger import RealFinger
 
-np.set_printoptions(suppress=True, precision=3)
 
-
-# Connect to the leg.
 finger = RealFinger.create('can0', 'can1')
-# finger.pause()
-# observation = finger.get_observation(finger.current_time_index())
 finger.current_time_index()
 
-ipdb.set_trace()
+desired_position = np.ones(3)
+kp = 5
+kd = 0
 
-time.sleep(10)
+def control_loop():
+    desired_torque = np.zeros(3)
+    while True:
+        desired_position = np.random.rand(3) * 6 - 1
+        for _ in range(300):
+            t = finger.append_desired_action(desired_torque)
+            desired_torque = kp * (desired_position - finger.get_observation(t).angle) - \
+                kd * finger.get_observation(t).velocity
+
+thread = threading.Thread(target=control_loop)
+
+thread.start()
+thread.join()
