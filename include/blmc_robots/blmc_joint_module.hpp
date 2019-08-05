10ac7c21b31bc6b39ba56d74ff5762d5422d9ef4
Maximilien Naveau <maximilien.naveau@tuebingen.mpg.de>
2019-08-05 10:47:25 +0200

    update the copyrights

--- include/blmc_robots/blmc_joint_module.hpp
+++ include/blmc_robots/blmc_joint_module.hpp
@@ -3,7 +3,7 @@
  * @author Manuel Wuthrich
  * @author Maximilien Naveau (maximilien.naveau@gmail.com)
  * @license License BSD-3-Clause
- * @copyright Copyright (c) 2019, New York University and Max Planck Gesellshaft.
+ * @copyright Copyright (c) 2019, New York University and Max Planck Gesellschaft.
  * @date 2019-07-11
  */
 #pragma once
