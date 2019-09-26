7989e558561a4896c16a9721dc73269f1183c212
Felix Widmaier <felix.widmaier@tuebingen.mpg.de>
2019-09-26 11:38:56 +0200

    Rename and cleanup the print position script

--- demos/demo_finger_print_position.py
+++ demos/demo_finger_print_position.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-import time
+"""Send zero-torque commands to Finger robot and print joint positions."""
 import numpy as np
 
 import py_finger
@@ -14,19 +14,13 @@ finger = py_finger.Frontend(finger_data)
 
 finger_backend.calibrate()
 
-# FIXME this makes the application hang
+# TODO this makes the application hang
 #t = finger.get_current_time_index()
 #print("t = %d" % t)
 
-kp = 5
-kd = 0
 desired_torque = np.zeros(3)
 
 while True:
-
-    desired_position = np.random.rand(3) * 6 - 1
-    for _ in range(300):
-        t = finger.append_desired_action(desired_torque)
-        #trq = finger.get_observation(t).torque
-        trq = finger.get_observation(t).angle
-        print("\r%6.3f %6.3f %6.3f" % (trq[0], trq[1], trq[2]), end="")
+    t = finger.append_desired_action(desired_torque)
+    pos = finger.get_observation(t).angle
+    print("\r%6.3f %6.3f %6.3f" % (pos[0], pos[1], pos[2]), end="")
