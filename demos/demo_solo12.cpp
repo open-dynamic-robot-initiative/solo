a0445458a247ef8c55a349983b1680bf1cbaabed
Julian Viereck <jviereck@tuebingen.mpg.de>
2019-11-21 10:35:26 +0100

    First basic setup for solo12. No implementation yet

--- demos/demo_solo12.cpp
+++ demos/demo_solo12.cpp
@@ -1,29 +1,29 @@
 /**
- * \file demo_solo.cpp
- * \brief The use of the wrapper implementing a small pid controller.
- * \author Maximilien Naveau
- * \date 2018
+ * \file demo_solo12.cpp
+ * \brief Implements basic PD controller reading slider values.
+ * \author Julian Viereck
+ * \date 21 November 2019
  *
- * This file uses the Solo class in a small demo.
+ * This file uses the Solo12 class in a small demo.
  */
 
 
-#include "blmc_robots/solo.hpp"
+#include "blmc_robots/solo12.hpp"
 #include "blmc_robots/common_programs_header.hpp"
-
 
 using namespace blmc_robots;
 
-
 static THREAD_FUNCTION_RETURN_TYPE control_loop(void* robot_void_ptr)
 {
-    Solo& robot = *(static_cast<Solo*>(robot_void_ptr));
+    Solo12& robot = *(static_cast<Solo12*>(robot_void_ptr));
 
-    double kp = 5.0;
-    double kd = 1.0;
-    double max_range = M_PI;
-    Vector8d desired_joint_position;
-    Vector8d desired_torque;
+    // double kp = 5.0;
+    // double kd = 1.0;
+    // double max_range = M_PI;
+    Vector12d desired_joint_position;
+    Vector12d desired_torque;
+
+    desired_torque.setZero();
 
     Eigen::Vector4d sliders;
     Eigen::Vector4d sliders_filt;
@@ -43,6 +43,7 @@ static THREAD_FUNCTION_RETURN_TYPE control_loop(void* robot_void_ptr)
 
         // aquire the slider signal
         sliders = robot.get_slider_positions();
+
         // filter it
         for (unsigned i = 0; i < sliders_filt_buffer.size(); ++i)
         {
@@ -57,19 +58,19 @@ static THREAD_FUNCTION_RETURN_TYPE control_loop(void* robot_void_ptr)
                               (double)sliders_filt_buffer[i].size();
         }
 
-        // the slider goes from 0 to 1 so we go from -0.5rad to 0.5rad
-        for (unsigned i = 0; i < 4; ++i)
-        {
-            // desired_pose[i].push_back
-            desired_joint_position(2 * i) = max_range * (sliders_filt(i) - 0.5);
-            desired_joint_position(2 * i + 1) =
-                max_range * (sliders_filt(i) - 0.5);
-        }
+        // // the slider goes from 0 to 1 so we go from -0.5rad to 0.5rad
+        // for (unsigned i = 0; i < 4; ++i)
+        // {
+        //     // desired_pose[i].push_back
+        //     desired_joint_position(2 * i) = max_range * (sliders_filt(i) - 0.5);
+        //     desired_joint_position(2 * i + 1) =
+        //         max_range * (sliders_filt(i) - 0.5);
+        // }
 
-        // we implement here a small pd control at the current level
-        desired_torque =
-            kp * (desired_joint_position - robot.get_joint_positions()) -
-            kd * robot.get_joint_velocities();
+        // // we implement here a small pd control at the current level
+        // desired_torque =
+        //     kp * (desired_joint_position - robot.get_joint_positions()) -
+        //     kd * robot.get_joint_velocities();
 
         // Send the current to the motor
         robot.send_target_joint_torque(desired_torque);
@@ -79,9 +80,9 @@ static THREAD_FUNCTION_RETURN_TYPE control_loop(void* robot_void_ptr)
 
         if ((count % 1000) == 0)
         {
-            print_vector("des_joint_tau", desired_torque);
-            print_vector("    joint_pos", robot.get_joint_positions());
-            print_vector("des_joint_pos", desired_joint_position);
+            // print_vector("des_joint_tau", desired_torque);
+            // print_vector("    joint_pos", robot.get_joint_positions());
+            // print_vector("des_joint_pos", desired_joint_position);
         }
         ++count;
     }  // endwhile
@@ -90,20 +91,16 @@ static THREAD_FUNCTION_RETURN_TYPE control_loop(void* robot_void_ptr)
 
 int main(int, char**)
 {
+    real_time_tools::RealTimeThread thread;
     enable_ctrl_c();
 
-    real_time_tools::RealTimeThread thread;
-
-    Solo robot;
-
+    Solo12 robot;
     robot.initialize();
 
     rt_printf("controller is set up \n");
-
     thread.create_realtime_thread(&control_loop, &robot);
 
     rt_printf("control loop started \n");
-
     while (true)
     {
         real_time_tools::Timer::sleep_sec(0.01);
