50fa224ad4c675cb032d70e572b780cf0cad94bb
Julian Viereck <jviereck@tuebingen.mpg.de>
2019-11-25 14:52:33 +0100

    Fixing and testing demo_solo12, adding python bindings.

--- demos/demo_solo12.cpp
+++ demos/demo_solo12.cpp
@@ -13,28 +13,41 @@
 
 using namespace blmc_robots;
 
+void map_sliders(Eigen::Ref<Eigen::Vector4d> sliders, Eigen::Ref<Vector12d> sliders_out)
+{
+    for (int i = 0; i < 4; i++)
+    {
+        sliders_out(3 * i + 0) = sliders(0);
+        sliders_out(3 * i + 1) = sliders(1);
+        sliders_out(3 * i + 2) = 2. * (1. - sliders(1));
+    }
+}
+
 static THREAD_FUNCTION_RETURN_TYPE control_loop(void*)
 {
     Solo12 robot;
     // TODO: Forward commandline argument here.
-    robot.initialize("ens4", 4);
-
+    robot.initialize("ens4", 6);
     rt_printf("control loop started \n");
 
-    double kp = 5.0;
-    double kd = 1.0;
+    robot.set_max_joint_torques(0.5);
+
+    // Using conversion from PD gains from example.cpp
+    double kp = 3.0 * 9 * 0.025;
+    double kd = 0.1 * 9 * 0.025;
     double max_range = M_PI;
     Vector12d desired_joint_position;
+    Vector12d desired_torque_tmp;
     Vector12d desired_torque;
 
     desired_torque.setZero();
 
-    Eigen::Vector4d sliders;
-    Eigen::Vector4d sliders_filt;
-    Eigen::Vector4d sliders_zero;
+    Vector12d sliders;
+    Vector12d sliders_filt;
+    Vector12d sliders_zero;
+    std::array<bool, 12> motor_enabled;
 
-    std::vector<std::deque<double> > sliders_filt_buffer(
-        robot.get_slider_positions().size());
+    std::vector<std::deque<double> > sliders_filt_buffer(12);
     size_t max_filt_dim = 200;
     for (unsigned i = 0; i < sliders_filt_buffer.size(); ++i)
     {
@@ -42,7 +55,7 @@ static THREAD_FUNCTION_RETURN_TYPE control_loop(void*)
     }
 
     robot.acquire_sensors();
-    sliders_zero = robot.get_slider_positions();
+    map_sliders(robot.get_slider_positions(), sliders_zero);
 
     size_t count = 0;
     while (!CTRL_C_DETECTED)
@@ -50,8 +63,15 @@ static THREAD_FUNCTION_RETURN_TYPE control_loop(void*)
         // acquire the sensors
         robot.acquire_sensors();
 
-        // aquire the slider signal
-        sliders = robot.get_slider_positions();
+        // acquire the motor enabled signal.
+        motor_enabled = robot.get_motor_enabled();
+
+        // HACK: Only read sliders from card0 if motors report enabled.
+        if (motor_enabled[0] && motor_enabled[1])
+        {
+            // acquire the slider signal
+            map_sliders(robot.get_slider_positions(), sliders);
+        }
 
         // filter it
         for (unsigned i = 0; i < sliders_filt_buffer.size(); ++i)
@@ -68,27 +88,39 @@ static THREAD_FUNCTION_RETURN_TYPE control_loop(void*)
         }
 
         // the slider goes from 0 to 1 so we go from -0.5rad to 0.5rad
-        for (unsigned i = 0; i < 4; ++i)
+        for (unsigned i = 0; i < sliders_filt_buffer.size(); ++i)
         {
             // desired_pose[i].push_back
-            desired_joint_position(2 * i) =
+            desired_joint_position(i) =
                 max_range * (sliders_filt(i) - sliders_zero(i));
-            desired_joint_position(2 * i + 1) = desired_joint_position(2 * i);
         }
 
         // we implement here a small pd control at the current level
-        desired_torque =
+        desired_torque_tmp =
             kp * (desired_joint_position - robot.get_joint_positions()) -
             kd * robot.get_joint_velocities();
 
+        // // HACK: Due to unstable SPI, only update torque for enabled motors.
+        for (unsigned i = 0; i < sliders_filt_buffer.size(); ++i)
+        {
+            if (motor_enabled[i])
+            {
+                desired_torque(i) = desired_torque_tmp(i);
+            }
+        }
 
         // print -----------------------------------------------------------
         if ((count % 1000) == 0)
         {
-            print_vector("sliders_filt", sliders);
+            printf("\33[H\33[2J"); //clear screen
+            print_vector("sliders_filt", sliders_filt);
+            print_vector("sliders_zero", sliders_zero);
+            print_vector("sliders_raw ", robot.get_slider_positions());
             print_vector("des_joint_tau", desired_torque);
             print_vector("    joint_pos", robot.get_joint_positions());
+            print_vector("    joint_vel", robot.get_joint_velocities());
             print_vector("des_joint_pos", desired_joint_position);
+            fflush(stdout);
         }
         ++count;
 
