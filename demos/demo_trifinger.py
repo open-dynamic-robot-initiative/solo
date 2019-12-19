2211122d72274f9d02e150643e55a14ce225bf26
Felix Widmaier <felix.widmaier@tuebingen.mpg.de>
2019-12-19 12:59:08 +0100

    Refactor TriFinger demo
    Refactor the choreography function to be more compact and easier to
    maintain.
    Also extend a bit the sidewards movement to have more joints move there.

--- demos/demo_trifinger.py
+++ demos/demo_trifinger.py
@@ -18,59 +18,35 @@ def run_choreography(robot):
     """Move the legs in some hard-coded choreography."""
 
     def perform_step(position):
-        t = robot.frontend.append_desired_action(robot.Action(position=position))
+        t = robot.frontend.append_desired_action(
+            robot.Action(position=position))
         time.sleep(1)
 
     deg45 = np.pi / 4
 
+    pose_idle = [0, -deg45, -deg45]
+    pose_inward = [0, +deg45, +deg45]
+    pose_side_1 = [-deg45, -deg45, -deg45]
+    pose_side_2 = [0, -deg45, 0]
+    pose_side_3 = [deg45, -deg45, -deg45]
+
+
     while True:
-        perform_step([
-            0, -deg45, -deg45,
-            0, -deg45, -deg45,
-            0, -deg45, -deg45,
-        ])
+        # initial pose
+        perform_step(pose_idle * 3)
 
-        perform_step([
-            0, +deg45, +deg45,
-            0, -deg45, -deg45,
-            0, -deg45, -deg45,
-        ])
+        # one finger moving to the centre
+        perform_step(pose_inward + pose_idle + pose_idle)
+        perform_step(pose_idle + pose_inward + pose_idle)
+        perform_step(pose_idle + pose_idle + pose_inward)
 
-        perform_step([
-            0, -deg45, -deg45,
-            0, +deg45, +deg45,
-            0, -deg45, -deg45,
-        ])
+        # initial pose
+        perform_step(pose_idle * 3)
 
-        perform_step([
-            0, -deg45, -deg45,
-            0, -deg45, -deg45,
-            0, +deg45, +deg45,
-        ])
-
-        perform_step([
-            0, -deg45, -deg45,
-            0, -deg45, -deg45,
-            0, -deg45, -deg45,
-        ])
-
-        perform_step([
-            -deg45, -deg45, -deg45,
-            0, -deg45, -deg45,
-            0, -deg45, -deg45,
-        ])
-
-        perform_step([
-            0, -deg45, -deg45,
-            -deg45, -deg45, -deg45,
-            0, -deg45, -deg45,
-        ])
-
-        perform_step([
-            0, -deg45, -deg45,
-            0, -deg45, -deg45,
-            -deg45, -deg45, -deg45,
-        ])
+        # side-wards movement
+        perform_step(pose_side_1 + pose_side_2 + pose_side_3)
+        perform_step(pose_side_3 + pose_side_1 + pose_side_2)
+        perform_step(pose_side_2 + pose_side_3 + pose_side_1)
 
 
 def main():
@@ -85,4 +61,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
