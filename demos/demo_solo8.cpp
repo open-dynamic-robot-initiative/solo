32994b38537c7da982550154869e227d2a546cf2
jviereck@tuebingen.mpg.de <Julian Viereck>
2020-02-19 19:27:14 -0500

    Finish solo8 integration with micro_drivers

--- demos/demo_solo8.cpp
+++ demos/demo_solo8.cpp
@@ -14,7 +14,6 @@
 
 using namespace blmc_robots;
 
-
 static THREAD_FUNCTION_RETURN_TYPE control_loop(void*)
 {
     // Solo8& robot = *(static_cast<Solo8*>(robot_void_ptr));
@@ -90,91 +89,31 @@ static THREAD_FUNCTION_RETURN_TYPE control_loop(void*)
 
 int main(int argc, char** argv)
 {
-    // enable_ctrl_c();
+    enable_ctrl_c();
 
-    // if(argc != 2)
-    // {
-    //    throw std::runtime_error("Wrong number of argument: `./demo_solo8 network_id`.");
-    // }
-
-    // real_time_tools::RealTimeThread thread;
-
-    // // Solo8 robot;
-    // // robot.initialize(std::string(argv[1]));
-
-    // rt_printf("controller is set up \n");
-
-    // // thread.create_realtime_thread(&control_loop, &robot);
-    // thread.create_realtime_thread(&control_loop);
-
-    // rt_printf("control loop started \n");
-
-    // while (true)
-    // {
-    //     real_time_tools::Timer::sleep_sec(0.01);
-    // }
-
-    // thread.join();
-
-    // ----------------------------------------------------
-    // Rewrite the example.cpp using the solo8 robot code.
-
-    nice(-20); //give the process a high priority
-
-    Solo8 robot;
-    robot.initialize("eno1");
-
-    int N_SLAVES_CONTROLED = 4;
-
-    int cpt = 0;
-	int cpt_enabled = 0;
-	double dt = 0.001;
-	double t = 0;
-	double kp = 3.;
-	double kd = 0.05;
-	double iq_sat = 4.0;
-	double freq = 0.5;
-	double amplitude = M_PI;
-	double init_pos[N_SLAVES * 2] = {0};
-	int disable_counter[N_SLAVES * 2] = {0};
-	int state = 0;
-    Vector8d desired_joint_position;
-    Vector8d desired_torque;
-
-    desired_joint_position.setZero();
-
-    std::chrono::time_point<std::chrono::system_clock> last = std::chrono::system_clock::now();
-	while (!CTRL_C_DETECTED)
-	{
-		if (((std::chrono::duration<double>)(std::chrono::system_clock::now() - last)).count() > dt)
-		{
-			last = std::chrono::system_clock::now(); //last+dt would be better
-			cpt++;
-			t += dt;
-			robot.acquire_sensors();
-
-            //closed loop, position
-            cpt_enabled += 1;
-
-            // we implement here a small pd control at the current level
-            desired_torque =
-                kp * (desired_joint_position - robot.get_joint_positions()) -
-                kd * robot.get_joint_velocities();
-
-            // Send the current to the motor
-            robot.send_target_joint_torque(desired_torque);
-
-			if (cpt % 1000 == 0)
-			{
-                print_vector("des_joint_tau", desired_torque);
-                print_vector("    joint_pos", robot.get_joint_positions());
-			}
-
-		}
-		else
-		{
-			std::this_thread::yield();
-		}
+    if(argc != 2)
+    {
+       throw std::runtime_error("Wrong number of argument: `./demo_solo8 network_id`.");
     }
+
+    real_time_tools::RealTimeThread thread;
+
+    // Solo8 robot;
+    // robot.initialize(std::string(argv[1]));
+
+    rt_printf("controller is set up \n");
+
+    // thread.create_realtime_thread(&control_loop, &robot);
+    thread.create_realtime_thread(&control_loop);
+
+    rt_printf("control loop started \n");
+
+    while (true)
+    {
+        real_time_tools::Timer::sleep_sec(0.01);
+    }
+
+    thread.join();
+
     return 0;
 }
