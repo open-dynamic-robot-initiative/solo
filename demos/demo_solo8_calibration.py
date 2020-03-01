2cb9236f1bb0bb8c35bd8b22e97e95f2db8155e8
Julian Viereck <jviereck@tuebingen.mpg.de>
2020-03-01 16:35:42 -0500

    Finish standup calibration procedure

--- demos/demo_solo8_calibration.py
+++ demos/demo_solo8_calibration.py
@@ -18,13 +18,13 @@ signal.signal(signal.SIGINT, handler)
 
 ###
 
-abs_enc_indices = np.array([ 
-    0.48793072, -0.39736822,  0.2617748 , -0.37641401, 
+abs_enc_indices = np.array([
+    0.48793072, -0.39736822,  0.2617748 , -0.37641401,
     -0.46944608, 0.64938238, -0.51563708,  0.41886759
 ])
 
 zero_pos = np.array([
-  0.5, -0.5, 0.5, -0.5, 
+  0.5, -0.5, 0.5, -0.5,
   -0.5, 0.5, -0.5, -0.5
 ])
 
@@ -51,7 +51,7 @@ robot.initialize("eno1")
 P = 1.
 D = 0.01
 
-T = 2500
+T = 10000
 log_pos = np.zeros((T, 8))
 log_enc = np.zeros((T, 8))
 
@@ -65,24 +65,27 @@ class GotoPdController(object):
         self.pos_del = pos_to - pos_from
         self.duration = duration
         self.tau = np.zeros(8)
-        
+
     def run(self, rel_t, pos, vel, enc):
-        target = self.pos_from + rel_t / self.duration * self.pos_del
+        target = self.pos_from + min(rel_t / self.duration, 1.) * self.pos_del
         self.tau[:] = self.P * (target - pos) - self.D * vel
         return self.tau
-        
+
 class SetZeroPos(object):
     def __init__(self):
-        pass
-        
+        self.first_call = True
+
     def run(self, rel_t, pos, vel, enc):
-        if rel_t < 0.1:
+        if self.first_call:
+            self.first_call = False
+
             robot.set_zero_angles(enc - abs_enc_indices - zero_pos)
-        
+
 controllers = [
     {'from': 0, 'till': 1., 'ctrl': GotoPdController(P, D, np.zeros(8), target_pos, 1)},
     {'from': 1, 'till': 2., 'ctrl': GotoPdController(P, D, target_pos, np.zeros(8), 1)}
-    {'from': 2, 'till': 2.5, 'ctrl': SetZeroPos()}  
+    {'from': 2, 'till': 2.05, 'ctrl': SetZeroPos()}
+    {'from': 2.05, 'till': T/dt, 'ctrl': GotoPdController(P, D, zero_pos, np.zeros(8), 2)}
 ]
 
 it = 0
@@ -92,7 +95,7 @@ for it in range(T):
     robot.fill_joint_positions(pos)
     robot.fill_joint_velocities(vel)
     robot.fill_joint_encoder_indices(enc)
-    
+
     log_pos[it] = pos
     log_enc[it] = enc
 
