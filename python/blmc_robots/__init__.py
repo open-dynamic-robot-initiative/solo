d83361a478e5e9cba98883e7c483fdf80afc84ee
Felix Widmaier <luator@users.noreply.github.com>
2020-03-02 09:58:48 +0100

    Remove Finger-related files (#57)
    * Remove finger-related files (they are now found in robot_fingers).
    * Remove a few obsolete finger demos completely.
    * Remove obsolete logger.py and plot_data.py

--- python/blmc_robots/__init__.py
+++ python/blmc_robots/__init__.py
@@ -1,7 +1,1 @@
-from blmc_robots.robot import Robot, demo_print_position
-
 from blmc_robots.blmc_single_leg import *
-from blmc_robots.py_real_finger import *
-from blmc_robots.py_trifinger import *
-from blmc_robots.py_one_joint import *
-from blmc_robots.py_two_joint import *
